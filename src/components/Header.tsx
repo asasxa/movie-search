24db9098f246790b160a3785aa7574e92a465ba1
asasxa <asasxa@users.noreply.github.com>
2026-03-27 12:16:49 +0300

    i

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,14 @@
-import Menu from './Menu';
+import Menu from './Menu'
 
-export const Header = () => {
+type Props = {}
+
+const Header = (props: Props) => {
   return (
     <>
-      <h1 className='text-white text-center text-4xl mx-[auto] my-[3rem]'>
-        Поиск фильмов по каталогу IMDb
-      </h1>
+      <h1 className='text-white text-center text-4xl mx-[auto] my-[3rem]'>Поиск фильмов по каталогу IMDb</h1>
       <Menu />
     </>
-  );
-};
+  )
+}
 
 export default Header;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
-import Menu from './Menu';
+import Menu  from './Menu';
 
 export const Layout = () => {
   return (
